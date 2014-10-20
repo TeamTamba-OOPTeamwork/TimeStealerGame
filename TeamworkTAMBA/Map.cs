28ec8e06aa53d74c7e47bf18200df515c45fe18f
Andrey Bozhilov <andrei.bozhilov@gmail.com>
2014-10-20 17:55:39 +0300

    Read enemies and other obj from file

--- TeamworkTAMBA/Map.cs
+++ TeamworkTAMBA/Map.cs
@@ -28,11 +28,9 @@
         {
             for (int i = 0; i < 9; i++)
             {
-                
-                this.CharatersAndItems.Add(i + 1, new List<GameObject>(){
-                new Enemy(SpriteType.Enemy, new Point(240, 280), 0),
-                new Enemy(SpriteType.Enemy, new Point(80, 80), 0)
-                });
+                List<GameObject> list = new List<GameObject>();
+                list = this.InitiaizeItems("../../Sprites/spriteCharacter" + (i + 1).ToString("0#") + ".txt");
+                this.CharatersAndItems.Add(i + 1, list);
             }
         }
 
@@ -134,6 +132,106 @@
                 Console.WriteLine("The file could not be read:");
                 Console.WriteLine(e.Message);
             }
+        }
+
+        private List<GameObject> InitiaizeItems(string mapName)
+        {
+            List<GameObject> list = new List<GameObject>();
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(mapName))
+                {
+                    int col = 0;
+
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+
+                        for (int row = 0; row < line.Length; row++)
+                        {
+                            GameObject item = new GameObject();
+                            // -40 so we can hide first tile from screen
+                            var itemlocation = new Point(row * mapTileSize - 40, col * mapTileSize - 40);
+
+                            switch (line[row].ToString())
+                            {
+                                case "a":
+                                    item = new Homework(SpriteType.Enemy, itemlocation, 1);
+                                    break;
+                                case "b":
+                                    item = new Teamwork(SpriteType.Teamwork, itemlocation, 1);
+                                    break;
+                                case "c":
+                                    item = new Exam(SpriteType.Exam, itemlocation, 1);
+                                    break;
+
+                                //case "d":
+                                //    item = new Homework(SpriteType.Homework, itemlocation, 2);
+                                //    break;
+                                //case "e":
+                                //    item = new Teamwork(SpriteType.Teamwork, itemlocation, 2);
+                                //    break;
+                                //case "f":
+                                //    item = new Exam(SpriteType.Exam, itemlocation, 2);
+                                //    break;
+
+                                //case "g":
+                                //    item = new Homework(SpriteType.Homework, itemlocation, 3);
+                                //    break;
+                                //case "h":
+                                //    item = new Teamwork(SpriteType.Teamwork, itemlocation, 3);
+                                //    break;
+                                //case "i":
+                                //    item = new Exam(SpriteType.Exam, itemlocation, 3);
+                                //    break;
+
+                                //case "j":
+                                //    item = new Homework(SpriteType.Homework, itemlocation, 3);
+                                //    break;
+                                //case "k":
+                                //    item = new Teamwork(SpriteType.Teamwork, itemlocation, 3);
+                                //    break;
+                                //case "l":
+                                //    item = new Exam(SpriteType.Exam, itemlocation, 3);
+                                //    break;
+                                case "N":
+                                    item = new Nakov(SpriteType.Player, itemlocation, 0);
+                                    break;
+                                case "A":
+                                    item = new Nasko(SpriteType.Nasko, itemlocation, 0);
+                                    break;
+                                case "D":
+                                    item = new Nakov(SpriteType.Didko, itemlocation, 0);
+                                    break;
+                                case "V":
+                                    item = new Nakov(SpriteType.Vlado, itemlocation, 0);
+                                    break;
+                                case "K":
+                                    item = new Nakov(SpriteType.SuperVlado, itemlocation, 0);
+                                    break;
+                                case "T":
+                                    item = new Nakov(SpriteType.Tedi, itemlocation, 0);
+                                    break;
+                                case "C":
+                                    item = new Coffee(SpriteType.Coffee, itemlocation, 0);
+                                    break;
+                                default:
+                                    break;
+                            }
+                            list.Add(item);
+                        }
+                        col++;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+
+            return list;
         }
     }
 }
