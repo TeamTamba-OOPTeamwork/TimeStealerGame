8fd64406c1b97f1fb30655e7708c94c3de28f3ec
Yana Slavcheva <yana.slavcheva@gmail.com>
2014-10-22 04:30:47 +0300

    added Gate.cs

---
+++ TeamworkTAMBA/MapItems/StaticObjects/Gate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TeamworkTAMBA
+{
+    using System.Drawing;
+
+    public class Gate : StaticObjects
+    {
+        public Gate(SpriteType sriteType, Point location, int id) :
+            base(sriteType, location, id)
+        {
+
+        }
+    }
+}
