42d6f963ab23eb3cadd5024bc7d0b47bbf75b28f
Yana Slavcheva <yana.slavcheva@gmail.com>
2014-10-22 18:18:28 +0300

    added new classes

---
+++ TeamworkTAMBA/Characters/Friends/Time.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamworkTAMBA
+{
+    class Time: Friend
+    {
+        public Time(SpriteType sriteType, Point location, int id) : base(sriteType, location, id)
+        {
+        }
+    }
+}
