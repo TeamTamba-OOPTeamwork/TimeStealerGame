ec9b15361daa7b3b211135007e152335a08d7118
Kristiyan <zontak@mail.bg>
2014-10-22 23:39:04 +0300

    second commit

---
+++ TeamworkTAMBA/Exceptions/InvalidLineLength.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamworkTAMBA.Exceptions
+{
+    using System.Windows.Forms;
+
+    public class InvalidLineLengthException : Exception
+    {
+        public InvalidLineLengthException(string message) :
+            base(message)
+        {
+        }
+    }
+}
